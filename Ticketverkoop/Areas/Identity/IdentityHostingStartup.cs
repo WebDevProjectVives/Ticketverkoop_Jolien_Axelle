8e5bc757bc0d032790f878d840199300a6c13a7f
Jolien Staelens <jolien.staelens2@student.vives.be>
2020-04-07 14:04:00 +0200

    Commit: identity toevoegen

---
+++ Ticketverkoop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ticketverkoop.Data;
+
+[assembly: HostingStartup(typeof(Ticketverkoop.Areas.Identity.IdentityHostingStartup))]
+namespace Ticketverkoop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
